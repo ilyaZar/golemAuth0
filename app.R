809dacd9303a664ecf761e45965fdecb07be0fa4
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-04-18 10:06:14 +0200

    update app.R

--- app.R
+++ app.R
@@ -4,4 +4,10 @@
 
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
-golemAuth0::run_app() # add parameters here (if any)
+# golemAuth0::run_app() # add parameters here (if any)
+options("golem.app.prod" = TRUE, shiny.port = 8080,
+        auth0_config_file = system.file("app/_auth0.yml",
+                                        shiny.port = 8080,
+                                        auth0_disable = TRUE,
+                                        package = "golemAuth0"))
+golemAuth0::run_app_auth0()
