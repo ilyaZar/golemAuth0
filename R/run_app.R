ffd71e03b77a7b934cadf6c251a5a1d01713dfa6
IlyaZar <zarubin@wiso.uni-koeln.de>
2023-04-18 09:30:52 +0200

    add run app function

--- R/run_app.R
+++ R/run_app.R
@@ -26,3 +26,17 @@ run_app <- function(
     golem_opts = list(...)
   )
 }
+#' Run the Shiny Application
+#'
+#' @export
+#' @importFrom golem with_golem_options
+run_app_auth0 <- function(...) {
+  with_golem_options(
+    app = auth0::shinyAppAuth0(
+      ui = app_ui(),        # these parenthesis are important
+      server = app_server,
+      config_file = system.file("app/_auth0.yml", package = "golemAuth0")
+    ),
+    golem_opts = list(...)
+  )
+}
